709c2d71e6919e3f381d7a65f8cfd6d37feffbbf
Tobias Olsson <tobias@olsson.be>
2016-03-14 12:29:40 +0100

    Add test case

--- onfido-test/src/test/java/com/youcruit/onfido/api/CheckTest.java
+++ onfido-test/src/test/java/com/youcruit/onfido/api/CheckTest.java
@@ -29,7 +29,10 @@ import com.youcruit.onfido.api.checks.ReportRequest;
 import com.youcruit.onfido.api.checks.ResultStatus;
 import com.youcruit.onfido.api.http.FakeHttpClient;
 import com.youcruit.onfido.api.http.OnfidoHttpClient;
+import com.youcruit.onfido.api.report.Report;
 import com.youcruit.onfido.api.report.ReportClient;
+import com.youcruit.onfido.api.report.ReportId;
+import com.youcruit.onfido.api.report.ReportStatus;
 import com.youcruit.onfido.api.report.ReportType;
 
 public class CheckTest extends HttpIT {
@@ -51,10 +54,13 @@ public class CheckTest extends HttpIT {
     public void runAllSequentialTests() throws IOException {
 	if (client instanceof FakeHttpClient) {
 	    FakeHttpClient fakeClient = (FakeHttpClient) client;
-	    fakeClient.addResponse(URI.create("https://api.onfido.com/v1/applicants"), "{\"id\":\"7b60944c-0e26-4c86-bf49-6fdd9df26e9f\",\"created_at\":\"2016-03-14T10:42:13Z\",\"sandbox\":true,\"title\":null,\"first_name\":\"Foo\",\"middle_name\":\"FB\",\"last_name\":\"Bar\",\"email\":\"2dffb03a-f520-495b-9c6c-15c9823b577a@example.com\",\"gender\":null,\"dob\":\"1980-03-14\",\"telephone\":null,\"mobile\":null,\"country\":\"usa\",\"href\":\"/v1/applicants/7b60944c-0e26-4c86-bf49-6fdd9df26e9f\",\"id_numbers\":[{\"type\":\"ssn\",\"value\":\"404-35-0728\"}],\"addresses\":[]}");
-	    fakeClient.addResponse(URI.create("https://api.onfido.com/v1/applicants/7b60944c-0e26-4c86-bf49-6fdd9df26e9f/checks"), "{\"id\":\"be77adc0-1197-4cc1-a2b0-bc0e277da747\",\"created_at\":\"2016-03-14T10:42:28Z\",\"status\":\"awaiting_applicant\",\"redirect_uri\":null,\"type\":\"standard\",\"result\":null,\"sandbox\":true,\"report_type_groups\":[\"2685\"],\"results_uri\":\"https://onfido.com/dashboard/information_requests/324364\",\"download_uri\":\"https://onfido.com/dashboard/pdf/information_requests/324364\",\"form_uri\":null,\"href\":null,\"reports\":[{\"created_at\":\"2016-03-14T10:42:28Z\",\"href\":\"/v1/checks/be77adc0-1197-4cc1-a2b0-bc0e277da747/reports/ae4048a2-fcae-49d9-bd6d-835932b3dc50\",\"id\":\"ae4048a2-fcae-49d9-bd6d-835932b3dc50\",\"name\":\"national_criminal\",\"properties\":{},\"result\":null,\"status\":\"awaiting_data\",\"variant\":\"standard\",\"breakdown\":{}},{\"created_at\":\"2016-03-14T10:42:28Z\",\"href\":\"/v1/checks/be77adc0-1197-4cc1-a2b0-bc0e277da747/reports/f0992591-e46b-4d16-af06-c9219c9a283e\",\"id\":\"f0992591-e46b-4d16-af06-c9219c9a283e\",\"name\":\"county_criminal\",\"properties\":{},\"result\":null,\"status\":\"awaiting_data\",\"variant\":\"standard\",\"breakdown\":{}},{\"created_at\":\"2016-03-14T10:42:28Z\",\"href\":\"/v1/checks/be77adc0-1197-4cc1-a2b0-bc0e277da747/reports/21b1d02a-8f57-4713-b44a-6a6a7045dd36\",\"id\":\"21b1d02a-8f57-4713-b44a-6a6a7045dd36\",\"name\":\"identity\",\"properties\":{},\"result\":null,\"status\":\"awaiting_data\",\"variant\":\"standard\",\"breakdown\":{}}]}");
-	    fakeClient.addResponse(URI.create("https://onfido.com/dashboard/pdf/information_requests/324364.pdf"), "%PDF---------------------------------");
-	    fakeClient.addResponse(URI.create("https://api.onfido.com/v1/applicants/7b60944c-0e26-4c86-bf49-6fdd9df26e9f/checks/be77adc0-1197-4cc1-a2b0-bc0e277da747"), "{\"id\":\"be77adc0-1197-4cc1-a2b0-bc0e277da747\",\"created_at\":\"2016-03-14T10:42:28Z\",\"status\":\"awaiting_applicant\",\"redirect_uri\":null,\"type\":\"standard\",\"result\":null,\"sandbox\":true,\"report_type_groups\":[\"2685\"],\"results_uri\":\"https://onfido.com/dashboard/information_requests/324364\",\"download_uri\":\"https://onfido.com/dashboard/pdf/information_requests/324364\",\"form_uri\":null,\"href\":null,\"reports\":[\"ae4048a2-fcae-49d9-bd6d-835932b3dc50\",\"f0992591-e46b-4d16-af06-c9219c9a283e\",\"21b1d02a-8f57-4713-b44a-6a6a7045dd36\"]}");
+	    fakeClient.addResponse(URI.create("https://api.onfido.com/v1/applicants"), "{\"id\":\"723d3bc3-1a12-4058-8429-b1f536e8af1e\",\"created_at\":\"2016-03-14T11:15:21Z\",\"sandbox\":true,\"title\":null,\"first_name\":\"Foo\",\"middle_name\":\"FB\",\"last_name\":\"Bar\",\"email\":\"a344feff-aedc-49cf-b166-dfba2046e5ee@example.com\",\"gender\":null,\"dob\":\"1980-03-14\",\"telephone\":null,\"mobile\":null,\"country\":\"usa\",\"href\":\"/v1/applicants/723d3bc3-1a12-4058-8429-b1f536e8af1e\",\"id_numbers\":[{\"type\":\"ssn\",\"value\":\"404-35-0728\"}],\"addresses\":[]}");
+	    fakeClient.addResponse(URI.create("https://api.onfido.com/v1/applicants/723d3bc3-1a12-4058-8429-b1f536e8af1e/checks"), "{\"id\":\"63b8a616-17fa-48c2-ab8c-52d3b12ed388\",\"created_at\":\"2016-03-14T11:16:16Z\",\"status\":\"awaiting_applicant\",\"redirect_uri\":null,\"type\":\"standard\",\"result\":null,\"sandbox\":true,\"report_type_groups\":[\"2685\"],\"results_uri\":\"https://onfido.com/dashboard/information_requests/324424\",\"download_uri\":\"https://onfido.com/dashboard/pdf/information_requests/324424\",\"form_uri\":null,\"href\":null,\"reports\":[{\"created_at\":\"2016-03-14T11:16:16Z\",\"href\":\"/v1/checks/63b8a616-17fa-48c2-ab8c-52d3b12ed388/reports/60828a0a-d39b-4872-a00c-684fa16b6ed0\",\"id\":\"60828a0a-d39b-4872-a00c-684fa16b6ed0\",\"name\":\"national_criminal\",\"properties\":{},\"result\":null,\"status\":\"awaiting_data\",\"variant\":\"standard\",\"breakdown\":{}},{\"created_at\":\"2016-03-14T11:16:16Z\",\"href\":\"/v1/checks/63b8a616-17fa-48c2-ab8c-52d3b12ed388/reports/562a1893-26ef-4ced-914d-efeba43db74e\",\"id\":\"562a1893-26ef-4ced-914d-efeba43db74e\",\"name\":\"county_criminal\",\"properties\":{},\"result\":null,\"status\":\"awaiting_data\",\"variant\":\"standard\",\"breakdown\":{}},{\"created_at\":\"2016-03-14T11:16:16Z\",\"href\":\"/v1/checks/63b8a616-17fa-48c2-ab8c-52d3b12ed388/reports/ecf53a53-1bda-4454-9be3-eac2bf6f9ca9\",\"id\":\"ecf53a53-1bda-4454-9be3-eac2bf6f9ca9\",\"name\":\"identity\",\"properties\":{},\"result\":null,\"status\":\"awaiting_data\",\"variant\":\"standard\",\"breakdown\":{}}]}");
+	    fakeClient.addResponse(URI.create("https://api.onfido.com/v1/applicants/723d3bc3-1a12-4058-8429-b1f536e8af1e/checks/63b8a616-17fa-48c2-ab8c-52d3b12ed388"), "{\"id\":\"63b8a616-17fa-48c2-ab8c-52d3b12ed388\",\"created_at\":\"2016-03-14T11:16:16Z\",\"status\":\"awaiting_applicant\",\"redirect_uri\":null,\"type\":\"standard\",\"result\":null,\"sandbox\":true,\"report_type_groups\":[\"2685\"],\"results_uri\":\"https://onfido.com/dashboard/information_requests/324424\",\"download_uri\":\"https://onfido.com/dashboard/pdf/information_requests/324424\",\"form_uri\":null,\"href\":null,\"reports\":[\"60828a0a-d39b-4872-a00c-684fa16b6ed0\",\"562a1893-26ef-4ced-914d-efeba43db74e\",\"ecf53a53-1bda-4454-9be3-eac2bf6f9ca9\"]}");
+	    fakeClient.addResponse(URI.create("https://onfido.com/dashboard/pdf/information_requests/324424.pdf"), "%PDF---------------------------------");
+	    fakeClient.addResponse(URI.create("https://api.onfido.com/v1/checks/63b8a616-17fa-48c2-ab8c-52d3b12ed388/reports/60828a0a-d39b-4872-a00c-684fa16b6ed0"), "{\"created_at\":\"2016-03-14T11:16:16Z\",\"href\":\"/v1/checks/63b8a616-17fa-48c2-ab8c-52d3b12ed388/reports/60828a0a-d39b-4872-a00c-684fa16b6ed0\",\"id\":\"60828a0a-d39b-4872-a00c-684fa16b6ed0\",\"name\":\"national_criminal\",\"properties\":{},\"result\":null,\"status\":\"awaiting_data\",\"variant\":\"standard\",\"breakdown\":{}}");
+	    fakeClient.addResponse(URI.create("https://api.onfido.com/v1/checks/63b8a616-17fa-48c2-ab8c-52d3b12ed388/reports/562a1893-26ef-4ced-914d-efeba43db74e"), "{\"created_at\":\"2016-03-14T11:16:16Z\",\"href\":\"/v1/checks/63b8a616-17fa-48c2-ab8c-52d3b12ed388/reports/562a1893-26ef-4ced-914d-efeba43db74e\",\"id\":\"562a1893-26ef-4ced-914d-efeba43db74e\",\"name\":\"county_criminal\",\"properties\":{},\"result\":null,\"status\":\"awaiting_data\",\"variant\":\"standard\",\"breakdown\":{}}");
+	    fakeClient.addResponse(URI.create("https://api.onfido.com/v1/checks/63b8a616-17fa-48c2-ab8c-52d3b12ed388/reports/ecf53a53-1bda-4454-9be3-eac2bf6f9ca9"), "{\"created_at\":\"2016-03-14T11:16:16Z\",\"href\":\"/v1/checks/63b8a616-17fa-48c2-ab8c-52d3b12ed388/reports/ecf53a53-1bda-4454-9be3-eac2bf6f9ca9\",\"id\":\"ecf53a53-1bda-4454-9be3-eac2bf6f9ca9\",\"name\":\"ssn_trace\",\"properties\":{},\"result\":null,\"status\":\"awaiting_data\",\"variant\":\"standard\",\"breakdown\":{}}");
 	}
 
 	// Far from optimal, but since all the tests require creation of a new applicant and doesn't modify it, do them sequentially
@@ -62,12 +68,18 @@ public class CheckTest extends HttpIT {
 	CheckCreationResponse check = createCheck(applicantId);
 	assertEquals(ResultStatus.AWAITING_DATA, check.getStatus());
 
-	byte[] pdfReport = reportClient.getPdfReport(check.getDownloadUri());
-	String s = new String(pdfReport, 0, 20, "UTF-8");
-	assertTrue("Not pdf?", s.startsWith("%PDF"));
 	Check gotCheck = checkClient.getCheck(applicantId, check.getId());
 	assertEquals(ResultStatus.AWAITING_DATA, gotCheck.getStatus());
 	assertEquals(null, gotCheck.getResult());
+
+	byte[] pdfReport = reportClient.getPdfReport(check.getDownloadUri());
+	String s = new String(pdfReport, 0, 20, "UTF-8");
+	assertTrue("Not pdf?", s.startsWith("%PDF"));
+
+	for (String reportId : gotCheck.getReports()) {
+	    Report report = reportClient.getReport(gotCheck.getId(), new ReportId(reportId));
+	    assertEquals(ReportStatus.AWAITING_DATA, report.getStatus());
+	}
     }
 
     private CheckCreationResponse createCheck(ApplicantId applicantId) throws IOException {
